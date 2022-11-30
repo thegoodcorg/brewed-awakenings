cfe94ce16d043fc4cfe0ad425f6712dfc7896ff3
Jeremy Goodman <jeremygoodman52@gmail.com>
2022-11-30 10:25:30 -0600

    initial commit

---
+++ scripts/Orders.js
@@ -0,0 +1,95 @@
+import { getProducts, getEmployees, getOrders } from "./database.js"
+
+// Get copy of state for use in this module
+const products = getProducts()
+const employees = getEmployees()
+const orders = getOrders()
+
+
+// Function whose responsibility is to find the product for an order
+const findProduct = (order, allProducts) => {
+    let orderProduct = {}
+
+    for (const product of allProducts) {
+        if (product.id === order.productId) {
+            orderProduct = product
+        }
+    }
+
+    return orderProduct
+}
+
+// Function whose responsibility is to find the employee for an order
+const findEmployee = (order, allEmployees) => {
+    let orderEmployee = {}
+
+    for (const employee of allEmployees) {
+        if (employee.id === order.employeeId) {
+            orderEmployee = employee
+        }
+    }
+
+    return orderEmployee
+}
+
+export const Orders = () => {
+    let html = ""
+    html = "<ul>"
+
+    for (const order of orders) {
+        const employee = findEmployee(order, employees)
+        const product = findProduct(order, products)
+
+        html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        let itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [, productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} is $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
+const employeeOrders = (employee) => {
+    let fulfilledOrders = 0
+
+    for (const order of orders) {
+        if (order.employeeId === employee.id) {
+            // Increment the number of fulfilled orders
+            fulfilledOrders += 1
+        }
+    }
+    return fulfilledOrders
+}
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        let itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const orderCount = employeeOrders(employee)
+                    window.alert(`${employee.name} has sold ${orderCount} products`)
+                }
+            }
+        }
+    }
+)
